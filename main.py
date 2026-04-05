34755369c385b74f62c2858667f0ee169fad4358
PoProstuFilip14 <f.romanowski@hotmail.com>
2026-04-05 15:23:39 +0200

    Checking colors in 9 positions

--- main.py
+++ main.py
@@ -34,40 +34,41 @@ while (1):
         white_upper = np.array([255, 100, 255], np.uint8)
         white_mask = cv2.inRange(hsvFrame, white_lower, white_upper)
 
-        kernal = np.ones((5, 5), "uint8")
+        middle = [int(imageFrame.shape[1] / 2), int(imageFrame.shape[0] / 2)]
+        gap = 70
 
-        red_mask = cv2.dilate(red_mask, kernal)
-        res_red = cv2.bitwise_and(imageFrame, imageFrame, mask=red_mask)
+        targets = [[middle[0] - gap, middle[1] - gap],
+                  [middle[0], middle[1] - gap],
+                  [middle[0] + gap, middle[1] - gap],
+                  [middle[0] - gap, middle[1]],
+                  [middle[0], middle[1]],
+                  [middle[0] + gap, middle[1]],
+                  [middle[0] - gap, middle[1] + gap],
+                  [middle[0], middle[1] + gap],
+                  [middle[0] + gap, middle[1] + gap]]
 
-        green_mask = cv2.dilate(green_mask, kernal)
-        res_green = cv2.bitwise_and(imageFrame, imageFrame, mask=green_mask)
+        print(hsvFrame[targets[5][1], targets[5][0]])
 
-        blue_mask = cv2.dilate(blue_mask, kernal)
-        res_blue = cv2.bitwise_and(imageFrame, imageFrame, mask=blue_mask)
-
-        target = [int(imageFrame.shape[1] / 2), int(imageFrame.shape[0] / 2)]
-
-        print(hsvFrame[target[1], target[0]])
-
-        if red_mask[target[1], target[0]] > 0:
-            imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (0, 0, 255), 8)
-        elif green_mask[target[1], target[0]] > 0:
-            imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (0, 255, 0), 8)
-        elif blue_mask[target[1], target[0]] > 0:
-            imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (255, 0, 0), 8)
-        elif orange_mask[target[1], target[0]] > 0:
-            imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (0, 122, 255), 8)
-        elif yellow_mask[target[1], target[0]] > 0:
-            imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (0, 255, 255), 8)
-        elif white_mask[target[1], target[0]] > 0:
-            imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (255, 255, 255), 8)
-        else:
-            imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 10, (0, 0, 0), 8)
+        for target in targets:
+            if red_mask[target[1], target[0]] > 0:
+                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (0, 0, 255), 8)
+            elif green_mask[target[1], target[0]] > 0:
+                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (0, 255, 0), 8)
+            elif blue_mask[target[1], target[0]] > 0:
+                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (255, 0, 0), 8)
+            elif orange_mask[target[1], target[0]] > 0:
+                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (0, 122, 255), 8)
+            elif yellow_mask[target[1], target[0]] > 0:
+                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (0, 255, 255), 8)
+            elif white_mask[target[1], target[0]] > 0:
+                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (255, 255, 255), 8)
+            else:
+                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 10, (0, 0, 0), 8)
 
         cv2.imshow("Color Detection", imageFrame)
 
     else:
-        print("Image reading error!!!");
+        print("Image reading error!!!")
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
         webcam.release()
