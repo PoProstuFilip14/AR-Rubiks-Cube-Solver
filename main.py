9b26a5a6b1cc85a8bc4658b31ac17d67fa1387d0
PoProstuFilip14 <f.romanowski@hotmail.com>
2026-04-07 13:33:06 +0200

    Taking photos for testing

--- main.py
+++ main.py
@@ -1,141 +1,246 @@
 import numpy as np
 import cv2
 import kociemba
+import datetime
 from collections import Counter
 
-webcam = cv2.VideoCapture(0)
-counter = 0
 cube = [[[-1 for _ in range(3)] for _ in range(3)] for _ in range(6)]
-isSaved = [False] * 6
-wallCounter = 0
+middle = [int(0), int(0)]
+gap = 100
+targets = [[middle[0] - gap, middle[1] - gap, -1],
+          [middle[0], middle[1] - gap, -1],
+          [middle[0] + gap, middle[1] - gap, -1],
+          [middle[0] - gap, middle[1], -1],
+          [middle[0], middle[1], -1],
+          [middle[0] + gap, middle[1], -1],
+          [middle[0] - gap, middle[1] + gap, -1],
+          [middle[0], middle[1] + gap, -1],
+          [middle[0] + gap, middle[1] + gap, -1]]
+is_recognized = True
 
-while (wallCounter < 6):
-    ret, imageFrame = webcam.read()
-    if ret:
-        hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
+def main():
+    exit = False
 
-        red_lower = np.array([0, 90, 2], np.uint8)
-        red_upper = np.array([7, 255, 255], np.uint8)
-        red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
+    while not exit:
+        option = input()
+        if option == '1':
+            cube_solver()
+        elif option == '2':
+            take_photos()
+        elif option == '3':
+            test_photos()
+        elif option == '4':
+            exit = True
 
-        green_lower = np.array([70, 90, 2], np.uint8)
-        green_upper = np.array([95, 255, 255], np.uint8)
-        green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
+def take_photos():
+    is_accepted = False
+    counter = 0
+    cam = cv2.VideoCapture(0)
 
-        blue_lower = np.array([95, 90, 2], np.uint8)
-        blue_upper = np.array([120, 255, 255], np.uint8)
-        blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
+    while 1:
+        ret, frame = cam.read()
 
-        orange_lower = np.array([7, 90, 2], np.uint8)
-        orange_upper = np.array([20, 255, 255], np.uint8)
-        orange_mask = cv2.inRange(hsvFrame, orange_lower, orange_upper)
+        if counter == 10:
+            cam.release()
+            cv2.destroyWindow("Captured")
+            break
+        if ret:
+            frame = cv2.flip(frame, 1)
 
-        yellow_lower = np.array([20, 90, 2], np.uint8)
-        yellow_upper = np.array([30, 255, 255], np.uint8)
-        yellow_mask = cv2.inRange(hsvFrame, yellow_lower, yellow_upper)
+            if not is_accepted:
+                middle = [int(frame.shape[1] / 2), int(frame.shape[0] / 2)]
+                gap = 100
 
-        white_lower = np.array([0, 0, 190], np.uint8)
-        white_upper = np.array([255, 90, 255], np.uint8)
-        white_mask = cv2.inRange(hsvFrame, white_lower, white_upper)
+                targets = [[middle[0] - gap, middle[1] - gap, -1],
+                           [middle[0], middle[1] - gap, -1],
+                           [middle[0] + gap, middle[1] - gap, -1],
+                           [middle[0] - gap, middle[1], -1],
+                           [middle[0], middle[1], -1],
+                           [middle[0] + gap, middle[1], -1],
+                           [middle[0] - gap, middle[1] + gap, -1],
+                           [middle[0], middle[1] + gap, -1],
+                           [middle[0] + gap, middle[1] + gap, -1]]
 
-        middle = [int(imageFrame.shape[1] / 2), int(imageFrame.shape[0] / 2)]
-        gap = 100
+                for target in targets:
+                    frame = cv2.circle(frame, [target[0], target[1]], 10, (0, 0, 0), 8)
 
-        targets = [[middle[0] - gap, middle[1] - gap, -1],
-                  [middle[0], middle[1] - gap, -1],
-                  [middle[0] + gap, middle[1] - gap, -1],
-                  [middle[0] - gap, middle[1], -1],
-                  [middle[0], middle[1], -1],
-                  [middle[0] + gap, middle[1], -1],
-                  [middle[0] - gap, middle[1] + gap, -1],
-                  [middle[0], middle[1] + gap, -1],
-                  [middle[0] + gap, middle[1] + gap, -1]]
-        isRecognized = True
+            cv2.imshow("Captured", frame)
 
-        #print(hsvFrame[targets[4][1], targets[4][0]])
+            if is_accepted:
+                filename = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+                cv2.imwrite(f"photos/{filename}_{counter}.png", frame)
+                counter += 1
 
-        for target in targets:
-            if red_mask[target[1], target[0]] > 0:
-                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (0, 0, 255), 8)
-                target[2] = 2
-            elif green_mask[target[1], target[0]] > 0:
-                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (0, 255, 0), 8)
-                target[2] = 4
-            elif blue_mask[target[1], target[0]] > 0:
-                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (255, 0, 0), 8)
-                target[2] = 1
-            elif orange_mask[target[1], target[0]] > 0:
-                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (0, 122, 255), 8)
-                target[2] = 5
-            elif yellow_mask[target[1], target[0]] > 0:
-                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (0, 255, 255), 8)
-                target[2] = 3
-            elif white_mask[target[1], target[0]] > 0:
-                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 5, (255, 255, 255), 8)
-                target[2] = 0
-            else:
-                imageFrame = cv2.circle(imageFrame, [target[0], target[1]], 10, (0, 0, 0), 8)
-                isRecognized = False
-
-
-        imageFrame = cv2.flip(imageFrame, 1)
-
-        cv2.imshow("Color Detection", imageFrame)
-
-        if isRecognized and not isSaved[targets[4][2]]:
-            counter += 1
         else:
-            counter = 0
+            print("Failed to capture image.")
 
-        if counter == 25:
-            #print(targets[0][2], targets[1][2], targets[2][2])
-            #print(targets[3][2], targets[4][2], targets[5][2])
-            #print(targets[6][2], targets[7][2], targets[8][2])
-            isSaved[targets[4][2]] = True
-            cube[targets[4][2]][0][0] = targets[0][2]
-            cube[targets[4][2]][0][1] = targets[1][2]
-            cube[targets[4][2]][0][2] = targets[2][2]
-            cube[targets[4][2]][1][0] = targets[3][2]
-            cube[targets[4][2]][1][1] = targets[4][2]
-            cube[targets[4][2]][1][2] = targets[5][2]
-            cube[targets[4][2]][2][0] = targets[6][2]
-            cube[targets[4][2]][2][1] = targets[7][2]
-            cube[targets[4][2]][2][2] = targets[8][2]
-            wallCounter += 1
-            print("Wall " + str(wallCounter) + " saved!!!")
+        key = cv2.waitKey(1) & 0xFF
 
-        cv2.imshow("Color Detection", imageFrame)
+        if key == ord('q'):
+            cam.release()
+            cv2.destroyWindow("Captured")
+            break
 
-    else:
-        print("Image reading error!!!")
+        if key == ord('0'):
+            is_accepted = True
 
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        webcam.release()
-        cv2.destroyAllWindows()
-        break
+def test_photos():
+    print("Testing photos...")
 
-cube_string = ""
-for wall in cube:
-    for row in wall:
-        for cell in row:
-            if cell == 0:
-                cube_string += 'U'
-            elif cell == 1:
-                cube_string += 'R'
-            elif cell == 2:
-                cube_string += 'F'
-            elif cell == 3:
-                cube_string += 'D'
-            elif cell == 4:
-                cube_string += 'L'
-            elif cell == 5:
-                cube_string += 'B'
+def read_colors(image_frame):
+    global targets, is_recognized
+
+    hsv_frame = cv2.cvtColor(image_frame, cv2.COLOR_BGR2HSV)
+
+    red_lower = np.array([0, 90, 2], np.uint8)
+    red_upper = np.array([7, 255, 255], np.uint8)
+    red_mask = cv2.inRange(hsv_frame, red_lower, red_upper)
+
+    red_lower = np.array([170, 90, 2], np.uint8)
+    red_upper = np.array([255, 255, 255], np.uint8)
+    red_mask_2 = cv2.inRange(hsv_frame, red_lower, red_upper)
+
+    green_lower = np.array([70, 60, 2], np.uint8)
+    green_upper = np.array([95, 255, 255], np.uint8)
+    green_mask = cv2.inRange(hsv_frame, green_lower, green_upper)
+
+    blue_lower = np.array([95, 90, 2], np.uint8)
+    blue_upper = np.array([120, 255, 255], np.uint8)
+    blue_mask = cv2.inRange(hsv_frame, blue_lower, blue_upper)
+
+    orange_lower = np.array([7, 90, 2], np.uint8)
+    orange_upper = np.array([20, 255, 255], np.uint8)
+    orange_mask = cv2.inRange(hsv_frame, orange_lower, orange_upper)
+
+    yellow_lower = np.array([20, 90, 2], np.uint8)
+    yellow_upper = np.array([30, 255, 255], np.uint8)
+    yellow_mask = cv2.inRange(hsv_frame, yellow_lower, yellow_upper)
+
+    white_lower = np.array([0, 0, 100], np.uint8)
+    white_upper = np.array([255, 90, 255], np.uint8)
+    white_mask = cv2.inRange(hsv_frame, white_lower, white_upper)
+
+    #print(hsv_frame[targets[4][1], targets[4][0]])
+
+    for target in targets:
+        if red_mask[target[1], target[0]] > 0 or red_mask_2[target[1], target[0]] > 0:
+            image_frame = cv2.circle(image_frame, [target[0], target[1]], 5, (0, 0, 255), 8)
+            target[2] = 2
+        elif green_mask[target[1], target[0]] > 0:
+            image_frame = cv2.circle(image_frame, [target[0], target[1]], 5, (0, 255, 0), 8)
+            target[2] = 4
+        elif blue_mask[target[1], target[0]] > 0:
+            image_frame = cv2.circle(image_frame, [target[0], target[1]], 5, (255, 0, 0), 8)
+            target[2] = 1
+        elif orange_mask[target[1], target[0]] > 0:
+            image_frame = cv2.circle(image_frame, [target[0], target[1]], 5, (0, 122, 255), 8)
+            target[2] = 5
+        elif yellow_mask[target[1], target[0]] > 0:
+            image_frame = cv2.circle(image_frame, [target[0], target[1]], 5, (0, 255, 255), 8)
+            target[2] = 3
+        elif white_mask[target[1], target[0]] > 0:
+            image_frame = cv2.circle(image_frame, [target[0], target[1]], 5, (255, 255, 255), 8)
+            target[2] = 0
+        else:
+            image_frame = cv2.circle(image_frame, [target[0], target[1]], 10, (0, 0, 0), 8)
+            is_recognized = False
+
+def generate_solution():
+    global cube
+    cube_string = ""
+    for wall in cube:
+        for row in wall:
+            for cell in row:
+                if cell == 0:
+                    cube_string += 'U'
+                elif cell == 1:
+                    cube_string += 'R'
+                elif cell == 2:
+                    cube_string += 'F'
+                elif cell == 3:
+                    cube_string += 'D'
+                elif cell == 4:
+                    cube_string += 'L'
+                elif cell == 5:
+                    cube_string += 'B'
+                else:
+                    print("ERROR!!!")
+
+    print(cube_string)
+    print(Counter(cube_string))
+
+    try:
+        cube_solution = kociemba.solve(cube_string)
+    except:
+        cube_solution = "Invalid cube!!!"
+
+    print(cube_solution)
+
+def cube_solver():
+    global cube, middle, gap, targets, is_recognized
+
+    counter = 0
+    is_saved = [False] * 6
+    wall_counter = 0
+
+    webcam = cv2.VideoCapture(0)
+
+    while wall_counter < 6:
+        ret, image_frame = webcam.read()
+        if ret:
+            middle = [int(image_frame.shape[1] / 2), int(image_frame.shape[0] / 2)]
+            gap = 100
+
+            targets = [[middle[0] - gap, middle[1] - gap, -1],
+                      [middle[0], middle[1] - gap, -1],
+                      [middle[0] + gap, middle[1] - gap, -1],
+                      [middle[0] - gap, middle[1], -1],
+                      [middle[0], middle[1], -1],
+                      [middle[0] + gap, middle[1], -1],
+                      [middle[0] - gap, middle[1] + gap, -1],
+                      [middle[0], middle[1] + gap, -1],
+                      [middle[0] + gap, middle[1] + gap, -1]]
+            is_recognized = True
+
+            read_colors(image_frame)
+
+            image_frame = cv2.flip(image_frame, 1)
+
+            cv2.imshow("Color Detection", image_frame)
+
+            if is_recognized and not is_saved[targets[4][2]]:
+                counter += 1
             else:
-                print("ERROR!!!")
+                counter = 0
 
-print(cube_string)
-print(Counter(cube_string))
+            if counter == 25:
+                #print(targets[0][2], targets[1][2], targets[2][2])
+                #print(targets[3][2], targets[4][2], targets[5][2])
+                #print(targets[6][2], targets[7][2], targets[8][2])
+                is_saved[targets[4][2]] = True
+                cube[targets[4][2]][0][0] = targets[0][2]
+                cube[targets[4][2]][0][1] = targets[1][2]
+                cube[targets[4][2]][0][2] = targets[2][2]
+                cube[targets[4][2]][1][0] = targets[3][2]
+                cube[targets[4][2]][1][1] = targets[4][2]
+                cube[targets[4][2]][1][2] = targets[5][2]
+                cube[targets[4][2]][2][0] = targets[6][2]
+                cube[targets[4][2]][2][1] = targets[7][2]
+                cube[targets[4][2]][2][2] = targets[8][2]
+                wall_counter += 1
+                print("Wall " + str(wall_counter) + " saved!!!")
 
-cube_solution = kociemba.solve(cube_string)
+            cv2.imshow("Color Detection", image_frame)
 
-print(cube_solution)
+        else:
+            print("Image reading error!!!")
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            webcam.release()
+            cv2.destroyAllWindows()
+            break
+
+    generate_solution()
+
+if __name__ == '__main__':
+    main()
